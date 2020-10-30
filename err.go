d72011c7f6a0d16188e60d5a420c3f9b409a0ec9
Ali Josie <aggr3ssor@protonmail.com>
2020-10-30 20:01:07 +0330

    add Must as an alias for CheckErr

--- err.go
+++ err.go
@@ -9,14 +9,13 @@ var (
 	ErrRepoNotExist = errors.New("repository does not exist")
 )
 
+// Must is an alias for CheckErr
 func Must(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+	CheckErr(err)
 }
 
 func CheckErr(err error) {
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
