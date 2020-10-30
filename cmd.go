3643b51e5c7242446eddafc007683e0acf0c3fc3
Ali Josie <aggr3ssor@protonmail.com>
2020-10-30 20:04:10 +0330

    add child fork handler

--- cmd.go
+++ cmd.go
@@ -19,50 +19,51 @@ func newVesselCommand() *cobra.Command {
 
 // newRunCommand implements and returns the run command.
 func newRunCommand() *cobra.Command {
-	ops := new(containerOptions)
-
 	cmd := &cobra.Command{
 		Use:                   "run [OPTIONS] IMAGE [COMMAND] [ARG...]",
 		Short:                 "Run a command inside a new container.",
 		DisableFlagsInUseLine: true,
 		Args:                  cobra.MinimumNArgs(1),
 		PreRunE: func(cmd *cobra.Command, args []string) (err error) {
-			src := args[0]
-			ops.image, err = newImage(src)
-			if err != nil {
-				cmd.SilenceUsage = true
-				return ErrRepoNotExist
-			}
-
-			// Convert memory and swap limit to megabyte
-			ops.mem *= MB
-			ops.swap *= MB
 			return
 		},
-		Run: func(cmd *cobra.Command, args []string) {
-			var (
-				Cmd string
-				Args []string
-			)
-			if len(args) > 1 {
-				Cmd = args[1]
-			}
-			if len(args) > 2 {
-				Args = args[2:]
-			}
+		Run: runRun,
+	}
 
-			runRun(ops, Cmd, Args...)
+	flags := cmd.Flags()
+	flags.StringP("name", "", "", "Container name")
+	flags.StringP("host", "", "", "Container Hostname")
+	flags.IntP("memory", "m", 100, "Limit memory access in MB")
+	flags.IntP("swap", "s", 20, "Limit swap access in MB")
+	flags.Float64P("cpus", "c", 2, "Limit CPUs")
+	flags.IntP("pids", "p", 100, "Limit number of processes")
+	flags.BoolP("detach", "d", false, "run command in the background")
+
+	return cmd
+}
+
+func newForkCommand() *cobra.Command {
+	ctr := new(container)
+	cmd := &cobra.Command{
+		Use:    "fork",
+		//Hidden: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			// Convert memory and swap limit to megabyte
+			ctr.mem *= MB
+			ctr.swap *= MB
+			runFork(ctr, args[0], args)
 		},
 	}
 
 	flags := cmd.Flags()
-	flags.StringVarP(&ops.name, "name", "", "", "Container name")
-	flags.StringVarP(&ops.hostname, "host", "", "", "Container Hostname")
-	flags.IntVarP(&ops.mem, "memory", "m", 100, "Limit memory access in MB")
-	flags.IntVarP(&ops.swap, "swap", "s", 20, "Limit swap access in MB")
-	flags.Float64VarP(&ops.cpus, "cpus", "c", 2, "Limit CPUs")
-	flags.IntVarP(&ops.pid, "pids", "p", 100, "Limit number of processes")
-	flags.BoolVarP(&ops.detach, "detach", "d", false, "run command in the background")
+	flags.StringVar(&ctr.image, "image", "", "Container name")
+	flags.StringVarP(&ctr.name, "name", "", "", "Container name")
+	flags.StringVarP(&ctr.hostname, "host", "", "", "Container Hostname")
+	flags.IntVarP(&ctr.mem, "memory", "m", 100, "Limit memory access in MB")
+	flags.IntVarP(&ctr.swap, "swap", "s", 20, "Limit swap access in MB")
+	flags.Float64VarP(&ctr.cpus, "cpus", "c", 2, "Limit CPUs")
+	flags.IntVarP(&ctr.pids, "pids", "p", 128, "Limit number of processes")
+	flags.BoolVarP(&ctr.detach, "detach", "d", false, "run command in the background")
 
 	return cmd
 }
