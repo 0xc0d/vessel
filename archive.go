fba4b9d0cea179b1cf62352215ee6beeda5c3e0b
Ali Josie <aggr3ssor@protonmail.com>
2020-10-31 07:00:18 +0330

    add link extract support

--- archive.go
+++ archive.go
@@ -12,12 +12,15 @@ func unTar(r io.Reader, dst string) error {
 		return err
 	}
 
+	hardLinks := make(map[string]string)
 	tarReader := tar.NewReader(r)
+
+loop:
 	for {
 		header, err := tarReader.Next()
 		switch {
 		case err == io.EOF:
-			return nil
+			break loop
 		case err != nil:
 			return err
 		case header == nil:
@@ -34,17 +37,36 @@ func unTar(r io.Reader, dst string) error {
 			}
 		case tar.TypeReg:
 			file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
-			if os.IsExist(err) {
-				continue
-			}
-			if err != nil {
+			switch {
+			case os.IsExist(err):
+				continue loop
+			case err != nil:
 				return err
 			}
+
 			if _, err = io.Copy(file, tarReader); err != nil {
 				return err
 			}
 			file.Close()
+		case tar.TypeLink:
+			/* Store hardlinks for further finally*/
+			link := filepath.Join(dst, header.Name)
+			linkTarget := filepath.Join(dst, header.Linkname)
+			hardLinks[link] = linkTarget
+		case tar.TypeSymlink:
+			linkPath := filepath.Join(dst, header.Name)
+			if err := os.Symlink(header.Linkname, linkPath); err != nil {
+				if !os.IsExist(err) {
+					return err
+				}
+			}
 		}
 	}
-}
 
+	for link, linkTarget := range hardLinks {
+		if err := os.Link(link, linkTarget); err != nil {
+			return err
+		}
+	}
+	return nil
+}
