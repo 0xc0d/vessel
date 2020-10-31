4a730169216d71b64b69ca87aef0f3a3e3e88fff
Ali Josie <aggr3ssor@protonmail.com>
2020-10-31 07:56:37 +0330

    add mount from image method

--- container.go
+++ container.go
@@ -2,38 +2,77 @@ package main
 
 import (
 	"crypto/sha256"
+	"fmt"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"math/rand"
+	"os"
+	"path/filepath"
+	"sync"
 	"syscall"
 	"time"
 )
 
 type container struct {
-	image    string
 	digest   string
 	name     string
 	hostname string
+	env      []string
 	mem      int
 	swap     int
 	pids     int
 	cpus     float64
 	detach   bool
+	once     sync.Once
 }
 
+const (
+	CtrDir = "/home/aggy/var/lib/vessel/containers"
+)
+
 func (c *container) setDigest() {
-	rand.Seed(time.Now().Unix())
-	randBuffer := make([]byte, 32)
-	rand.Read(randBuffer)
-	sha := sha256.New().Sum(randBuffer)
-	c.digest = string(sha)
+	c.once.Do(func() {
+		if len(c.digest) > 0 {
+			return
+		}
+		rand.Seed(time.Now().Unix())
+		randBuffer := make([]byte, 32)
+		rand.Read(randBuffer)
+		sha := sha256.New().Sum(randBuffer)
+		c.digest = fmt.Sprintf("%x", sha)[:64]
+	})
 }
 
 func (c *container) setHostname() {
 	if c.hostname == "" {
-		c.hostname = c.image[:12]
+		c.hostname = c.digest[:12]
 	}
 	Must(syscall.Sethostname([]byte(c.hostname)))
 }
 
-func (c *container) loadCGroups() (remover, error) {
+func (c *container) loadCGroups() error {
 	return newCGroup(c).Load()
+}
+
+func (c *container) mountFromImage(img v1.Image) error {
+	mountPoint := filepath.Join(CtrDir, c.digest, "mnt")
+	if err := os.MkdirAll(mountPoint, 0755); err != nil {
+		return err
+	}
+	layers, err := getImageLayers(img)
+	if err != nil {
+		return err
+	}
+	if err := overlayFsMount(mountPoint, layers, false); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *container) unmountFs() error {
+	containerPath := filepath.Join(CtrDir, c.digest)
+	mountPoint := filepath.Join(containerPath, "mnt")
+	if err := syscall.Unmount(mountPoint, 0); err != nil {
+		return err
+	}
+	return os.RemoveAll(containerPath)
 }
