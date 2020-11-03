59cf2214bc6f64d8b000acf107d619f741994a2e
Ali Josie <aggr3ssor@protonmail.com>
2020-11-03 04:05:38 +0330

    update image downloading

--- pkg/image/download.go
+++ pkg/image/download.go
@@ -1,102 +1,55 @@
 package image
 
 import (
+	"encoding/json"
 	"github.com/0xc0d/vessel/pkg/archive"
-	"github.com/google/go-containerregistry/pkg/crane"
-	"github.com/google/go-containerregistry/pkg/v1"
 	"os"
 	"path/filepath"
 )
 
-// download downloads tar format of image in TmpDir.
-// It then extract the image and copy it into ImgDir
-func Download(img v1.Image) error {
-	imageDigest, err := img.Digest()
+type Repository map[string]string
+type Repositories map[string]Repository
+
+func (i *Image) Download() error {
+	layers, err := i.Layers()
 	if err != nil {
 		return err
 	}
-
-	imageTmpPath := filepath.Join(TmpDir, imageDigest.Hex) + tarExt
-	if err := crane.Save(img, imageDigest.Hex, imageTmpPath); err != nil {
-		return err
-	}
-	if err := extract(img); err != nil {
-		return err
-	}
-
-	return remove(img)
-}
-
-// extract extracts the downloaded image
-func extract(img v1.Image) error {
-	imageDigest, err := img.Digest()
-	if err != nil {
-		return err
-	}
-
-	imageTempDir := filepath.Join(TmpDir, imageDigest.Hex)
-	imageFile := imageTempDir + tarExt
-
-	tar, err := archive.NewTarFile(imageFile)
-	if err != nil {
-		return err
-	}
-	if err := tar.Extract(imageTempDir); err != nil {
-		return err
-	}
-
-	// extract image layers
-	if err := layerExtract(img); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// layerExtract extracts tar.gz layerFiles inside image
-func layerExtract(img v1.Image) error {
-	imageDigest, err := img.Digest()
-	if err != nil {
-		return err
-	}
-	manifest, err := img.Manifest()
-	if err != nil {
-		return err
-	}
-
-	ch := make(chan error, len(manifest.Layers))
-	for _, layer := range manifest.Layers {
-		layerFilepath := filepath.Join(TmpDir, imageDigest.Hex, layer.Digest.Hex) + tarGzExt
-		tarGz, err := archive.NewTarGzFile(layerFilepath)
+	for _, layer := range layers {
+		digest, err := layer.Digest()
 		if err != nil {
 			return err
 		}
-		dst := filepath.Join(ImgDir, imageDigest.Hex, layer.Digest.Hex)
-		go func() {
-			ch <- tarGz.Extract(dst)
-		}()
-	}
+		rc, err := layer.Uncompressed()
+		if err != nil {
+			return err
+		}
 
-	for i := 0; i < len(manifest.Layers); i++ {
-		err := <-ch
+		tarball := archive.NewTarExtractor(rc)
+		err = tarball.Extract(filepath.Join(LyrDir, digest.Hex))
+		rc.Close()
 		if err != nil {
 			return err
 		}
 	}
-
-	return nil
+	return i.addToRepositories()
 }
 
-func remove(img v1.Image) error {
-	imageDigest, err := img.Digest()
+func (i *Image) addToRepositories() error {
+	file, err := os.OpenFile(RepoFile, os.O_RDWR|os.O_CREATE, 0600)
 	if err != nil {
 		return err
 	}
+	defer file.Close()
+	repos := make(Repositories)
+	decoder := json.NewDecoder(file)
+	decoder.Decode(&repos)
 
-	imageTempDir := filepath.Join(TmpDir, imageDigest.Hex)
-	imageFile := imageTempDir + tarExt
-	if err := os.RemoveAll(imageTempDir); err != nil {
+	digest, err := i.Digest()
+	if err != nil {
 		return err
 	}
-	return os.RemoveAll(imageFile)
+	repos[i.Repository] = Repository{i.Name: digest.String()}
+	encoder := json.NewEncoder(file)
+	return  encoder.Encode(repos)
 }
